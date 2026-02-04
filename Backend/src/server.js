9efff5bbb6ce2254a67d53fa6fd9b0f987db4aca
sushmach53 <sushmach33@gmail.com>
2026-02-04 23:56:15 +0530

    "deployment phase is executed"

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -45,6 +45,6 @@ app.get("*",(req,res) => {
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
+  console.log("Server is running on the port:", PORT);
 });
 });

cfdaae41ff4b353cf4bae6ecdfc7fa2b48904516
sushmach53 <sushmach33@gmail.com>
2026-02-04 23:47:51 +0530

    executed deployment phase

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesroutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,16 +12,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
 
-app.use(
+if(process.env.Node_ENV !== "production"){
+  app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+}
+
 app.use(express.json());
 app.use("/api/notes", rateLimiter, notesRoutes);
-
 
 
 // app.use(express.json())
@@ -30,8 +34,14 @@ app.use("/api/notes", rateLimiter, notesRoutes);
 // });
 
 
-
 app.use("/api/notes", notesRoutes);
+
+if(process.env.Node_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
